41a2da9898eb28ec5ff30d13f6506cecf1c82dee
lry <lry>
2019-06-24 12:54:33 +0800

    restructure

--- neural-core/src/main/resources/limiter_request.lua
+++ neural-core/src/main/resources/limiter_request.lua
@@ -2,20 +2,23 @@
 
 -- 获取限流KEY(LUA下标从1开始)
 local key = KEYS[1]
---- 时间窗最大请求数
-local max_request = tonumber(ARGV[1])
---- 时间窗大小
-local window_size = tonumber(ARGV[2])
+-- 时间窗口内最大并发数
+local max_permits = tonumber(ARGV[1])
+-- 窗口的间隔时间
+local interval_milliseconds = tonumber(ARGV[2])
 
---- 时间窗内当前请求数
-local current_request = tonumber(redis.call("get", key) or 0)
-if current_request + 1 > max_request then
+-- 获取当前的许可数
+local current_permits = tonumber(redis.call("GET", key) or 0)
+
+-- 如果超过了最大并发数，返回false
+if current_permits + 1 > max_permits then
     return 0
 else
-    redis.call("INCRBY", key,1)
-    if window_size > 0 then
-        redis.call("expire", key, window_size)
+    -- 增加并发计数
+    redis.call("INCRBY", key, 1)
+    -- 如果key中保存的并发计数为0，说明当前是一个新的时间窗口，它的过期时间设置为窗口的过期时间
+    if current_permits == 0 then
+        redis.call("PEXPIRE", key, interval_milliseconds)
     end
-    return current_request + 1
+    return current_permits + 1
 end
-
