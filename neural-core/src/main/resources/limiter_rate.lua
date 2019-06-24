ddd2ea4e4c61086ee339e42a8dde7b8843a5e99a
lry <595208882@qq.com>
2019-06-25 00:48:50 +0800

    restructure

--- neural-core/src/main/resources/limiter_rate.lua
+++ neural-core/src/main/resources/limiter_rate.lua
@@ -1,102 +1,79 @@
----
---- 基于Redis实现令牌桶限流
----
+---[[
+Rate Limiter Script v1.0
+@author：lry(echo)
+@since：2019-06-25
+@link https://github.com/ZhuBaker/distributed-current-limiter/blob/master/src/main/resources/lua/rate_limiter.lua
+@link https://github.com/herosea/priority-rate-limiter/blob/master/src/main/resources/rate_limiter.lua
+---]]
 
---- 1.获取令牌
---- 返回码:0=没有令牌桶配置,-1=表示取令牌失败，也就是桶里没有令牌,1=表示取令牌成功
+--- 判断source_str中是否包含sub_str
+--- @param source_str
+--- @param sub_str
+--- @return false=不包含,true=包含
+local function contains(source_str , sub_str)
+    local start_pos,end_pos = string.find(source_str,sub_str)
+    if start_pos == nil then
+        return false
+    end
+    local source_str_len = string.len(source_str)
+    local s = string.sub(source_str,end_pos,end_pos)
+    if source_str_len == end_pos then
+        return true
+    elseif string.sub(source_str,end_pos + 1,end_pos + 1) == ',' then
+        return true
+    end
+    return false
+end
+
+
+--- 获取令牌
 --- @param key 令牌的唯一标识
---- @param permits_s 请求令牌数量
---- @param curr_mill_second_s 当前毫秒数
---- @param reserved_percent_s 桶中预留的令牌百分比，整数
---- @param max_wait_mill_second_s 最长等待多久。负值意味着给其它请求保留部分token
-local function acquire(key, permits_s, curr_mill_second_s, reserved_percent_s, max_wait_mill_second_s)
-    local permits = tonumber(permits_s)
-    local curr_mill_second = tonumber(curr_mill_second_s)
-    local reserved_percent = tonumber(reserved_percent_s);
-    local max_wait_mill_second = 0
-    if reserved_percent <= 0 then
-        -- 不预留令牌时，等待时间才生效
-        max_wait_mill_second = tonumber(max_wait_mill_second_s)
+--- @param permits  请求令牌数量
+--- @param curr_mill_second 当前毫秒数
+--- @param app 使用令牌的应用标识
+--- @return -1=没有令牌桶配置,0=表示取令牌失败(也就是桶里没有令牌),1=表示获取令牌成功
+local function tryAcquire(key , permits , curr_mill_second , app)
+    local redis_rate_limit_info = redis.pcall('HMGET' , key , 'last_mill_second','curr_permits','max_permits','rate','apps')
+    local last_mill_second = redis_rate_limit_info[1]
+    local curr_permits = tonumber(redis_rate_limit_info[2])
+    local max_permits = tonumber(redis_rate_limit_info[3])
+    local rate = redis_rate_limit_info[4]
+    local apps = redis_rate_limit_info[5]
+
+    -- 标识没有配置令牌桶
+    if type(apps) == 'boolean' or apps == nil or not contains(apps , app) then
+        return -1
     end
 
-    local rate_limit_info = redis.pcall("HMGET", key, "last_mill_second", "curr_permits", "max_permits", "rate")
-    local last_mill_second = tonumber(rate_limit_info[1])
-    local curr_permits = tonumber(rate_limit_info[2])
-    local max_permits = tonumber(rate_limit_info[3])
-    local rate = tonumber(rate_limit_info[4])
-
-    --- 标识没有配置令牌桶
-    if type(rate) == 'boolean' or rate == nil then
-        return -2
-    end
-
-    local local_curr_permits = max_permits;
-    local secondToMill = 1000;
-    local percent = 100;
+    local local_curr_permits = curr_permits
 
     --- 令牌桶刚刚创建，上一次获取令牌的毫秒数为空
     --- 根据和上一次向桶里添加令牌的时间和当前时间差，触发式往桶里添加令牌，并且更新上一次向桶里添加令牌的时间
     --- 如果向桶里添加的令牌数不足一个，则不更新上一次向桶里添加令牌的时间
     if (type(last_mill_second) ~= 'boolean' and last_mill_second ~= nil) then
-        local reverse_permits, rest = math.modf(((curr_mill_second - last_mill_second) / secondToMill) * rate)
+        local reverse_permits = math.floor(((curr_mill_second - last_mill_second)/1000) * rate)
         local expect_curr_permits = reverse_permits + curr_permits;
-        local_curr_permits = math.min(expect_curr_permits, max_permits);
-
+        local_curr_permits = math.min(expect_curr_permits,max_permits)
         --- 大于0表示不是第一次获取令牌，也没有向桶里添加令牌
         if (reverse_permits > 0) then
-            redis.pcall("HSET", key, "last_mill_second", curr_mill_second - rest * secondToMill / rate)
+            redis.pcall('HSET',key, 'last_mill_second' , curr_mill_second)
         end
     else
-        redis.pcall("HSET", key, "last_mill_second", curr_mill_second)
+        redis.pcall('HSET' , key, 'last_mill_second' , curr_mill_second)
     end
 
-    local result = -1
-    local remainder = local_curr_permits - permits
-    if (remainder + rate * max_wait_mill_second / secondToMill - max_permits * reserved_percent / percent >= 0) then
-        -- https://redis.io/commands/eval always convert Lua numbers into integer replies
-        result = math.max(0, (-remainder * secondToMill) / rate)
-        redis.pcall("HSET", key, "curr_permits", remainder)
+    -- 判断许可数
+    if (local_curr_permits - permits >=0) then
+        -- 获取令牌成功
+        redis.pcall('HSET' , key , 'curr_permits' , local_curr_permits - permits)
+        return 1
     else
-        redis.pcall("HSET", key, "curr_permits", local_curr_permits)
+        -- 获取令牌失败
+        redis.pcall('HSET' , key , 'curr_permits' , local_curr_permits)
+        return 0
     end
-
-    return result
 end
 
+--- 主流程
+return tryAcquire(KEYS[1] , KEYS[2] , KEYS[3] , KEYS[4])
 
---- 2.初始化令牌桶配置
---- @param key 令牌的唯一标识
---- @param max_permits 桶大小
---- @param rate 向桶里添加令牌的速率
---- @param apps 可以使用令牌桶的应用列表，应用之前用逗号分隔
-local function init(key, max_permits, rate)
-    local rate_limit_info = redis.pcall("HMGET", key, "last_mill_second", "curr_permits", "max_permits", "rate")
-    local org_max_permits = tonumber(rate_limit_info[3])
-    local org_rate = rate_limit_info[4]
-
-    if (org_max_permits == nil) or (rate ~= org_rate or max_permits ~= org_max_permits) then
-        redis.pcall("HMSET", key, "max_permits", max_permits, "rate", rate, "curr_permits", max_permits)
-    end
-    return 1;
-end
-
-
---- 3.删除令牌桶
-local function delete(key)
-    redis.pcall("DEL", key)
-    return 1;
-end
-
-
-
-local key = KEYS[1]
-local method = KEYS[2]
-if method == "acquire" then
-    return acquire(key, ARGV[1], ARGV[2], ARGV[3], ARGV[4])
-elseif method == "init" then
-    return init(key, ARGV[1], ARGV[2])
-elseif method == "delete" then
-    return delete(key)
-else
-    -- ignore
-end
