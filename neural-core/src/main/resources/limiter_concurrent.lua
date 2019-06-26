5d85a25cf5fea76124ddeb085cb144ee9234adb5
lry <595208882@qq.com>
2019-06-26 09:31:13 +0800

    restructure

--- neural-core/src/main/resources/limiter_concurrent.lua
+++ neural-core/src/main/resources/limiter_concurrent.lua
@@ -3,7 +3,6 @@ Concurrent Limiter Script v1.0
 @author：lry(echo)
 @since：2019-06-25
 @TODO 死信问题怎么解决？
-获取限流KEY(LUA下标从1开始)
 ---]]
 
 
@@ -11,7 +10,7 @@ Concurrent Limiter Script v1.0
 --- @param key      唯一标识
 --- @param permits  大于0表示加流量,小于0表示减流量
 --- @param limit    获取限流大小参数
---- @return 0=表示取令牌失败(也就是桶里没有令牌),1=表示获取令牌成功
+--- @return         0=表示取令牌失败(也就是桶里没有令牌),1=表示获取令牌成功
 local function trafficConcurrent(key , permits, limit)
     -- 获取当前流量大小,没有则默认为0
     local currentConcurrent = tonumber(redis.call("GET", key) or "0")
