cb86c1b141da4b9a9028414d2801d04dbd34001f
lry <595208882@qq.com>
2019-06-27 23:57:05 +0800

    restructure

--- neural-core/src/test/java/org/micro/neural/config/RedisStoreTest.java
+++ neural-core/src/test/java/org/micro/neural/config/RedisStoreTest.java
@@ -1,26 +1,34 @@
 package org.micro.neural.config;
 
+import org.micro.neural.OriginalCall;
 import org.micro.neural.common.URL;
 import org.micro.neural.config.store.IStore;
 import org.micro.neural.config.store.StorePool;
-import org.micro.neural.limiter.core.ClusterLimiter;
+import org.micro.neural.limiter.Limiter;
+import org.micro.neural.limiter.LimiterConfig;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RedisStoreTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
         StorePool storePool = StorePool.getInstance();
         URL url = URL.valueOf("redis://127.0.0.1:6379");
         storePool.initialize(url);
         IStore store = storePool.getStore();
-        List<Object> keys = new ArrayList<>();
-        keys.add("testQuery");
-        keys.add(1);
-        keys.add(5);
-        List<Object> result = store.eval(ClusterLimiter.CONCURRENT_SCRIPT, 1000L, keys);
-        System.out.println(result);
+
+        String key1 = "testQuery";
+
+        Limiter limiter = new Limiter();
+        LimiterConfig limiterConfig1 = new LimiterConfig();
+        limiter.addConfig(limiterConfig1);
+        Object result = limiter.doWrapperCall(key1, new OriginalCall() {
+            @Override
+            public Object call() throws Throwable {
+                System.out.println("Input call");
+                return super.call();
+            }
+        });
     }
 
 }
