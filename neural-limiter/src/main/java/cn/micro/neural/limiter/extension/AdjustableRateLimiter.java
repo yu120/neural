e5205221cf536ded34b47ca658465a0c8c571c12
lry <595208882@qq.com>
2020-04-15 17:20:12 +0800

    adjust

---
+++ neural-limiter/src/main/java/cn/micro/neural/limiter/extension/AdjustableRateLimiter.java
@@ -0,0 +1,257 @@
+package cn.micro.neural.limiter.extension;
+
+import static java.lang.Math.max;
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import com.google.common.base.Stopwatch;
+import com.google.common.util.concurrent.Uninterruptibles;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The Adjustable Rate Limiter.
+ *
+ * @author lry
+ */
+public class AdjustableRateLimiter {
+
+    private static final long TIME_GRIT = 1L;
+    private volatile double timeGritSecond;
+
+    double getTimeGritSecond() {
+        return timeGritSecond;
+    }
+
+    private void setTimeGritSecond(long timeGritSecond) {
+        this.timeGritSecond = SECONDS.toMicros(timeGritSecond);
+    }
+
+    public static AdjustableRateLimiter create(long permitsPerSecond) {
+        return create(permitsPerSecond, TIME_GRIT);
+    }
+
+    public static AdjustableRateLimiter create(double permitsPerSecond, long timeGritSecond) {
+        return create(SleepingStopwatch.createFromSystemTimer(), permitsPerSecond, timeGritSecond);
+    }
+
+    /**
+     * The create adjustable rate limiter
+     *
+     * @param stopwatch        {@link SleepingStopwatch}
+     * @param permitsPerSecond permits per second
+     * @param timeGritSecond   time grit second
+     * @return {@link AdjustableRateLimiter}
+     */
+    private static AdjustableRateLimiter create(SleepingStopwatch stopwatch, double permitsPerSecond, long timeGritSecond) {
+        AdjustableRateLimiter adjustableRateLimiter = new SmoothRateLimiter.SmoothBursty(stopwatch, 1.0);
+        adjustableRateLimiter.setTimeGritSecond(timeGritSecond);
+        adjustableRateLimiter.setRate(permitsPerSecond);
+        return adjustableRateLimiter;
+    }
+
+    public static AdjustableRateLimiter create(double permitsPerSecond, long warmupPeriod, TimeUnit unit) {
+        return create(permitsPerSecond, warmupPeriod, unit, TIME_GRIT);
+    }
+
+    public static AdjustableRateLimiter create(
+            double permitsPerSecond, long warmUpPeriod, TimeUnit unit, long timeGritSecond) {
+        if (warmUpPeriod < 0) {
+            throw new IllegalArgumentException(String.format("warmupPeriod must not be negative: %s", warmUpPeriod));
+        }
+        return create(SleepingStopwatch.createFromSystemTimer(),
+                permitsPerSecond, warmUpPeriod, unit, 3.0, timeGritSecond);
+    }
+
+    /**
+     * The create adjustable rate limiter
+     *
+     * @param stopwatch        {@link SleepingStopwatch}
+     * @param permitsPerSecond permits per second
+     * @param warmUpPeriod     warm up period
+     * @param unit             {@link TimeUnit}
+     * @param coldFactor       cold factor
+     * @param timeGritSecond   time grit second
+     * @return {@link AdjustableRateLimiter}
+     */
+    private static AdjustableRateLimiter create(SleepingStopwatch stopwatch, double permitsPerSecond,
+                                                long warmUpPeriod, TimeUnit unit, double coldFactor, long timeGritSecond) {
+        AdjustableRateLimiter adjustableRateLimiter = new SmoothRateLimiter.SmoothWarmingUp(stopwatch, warmUpPeriod, unit, coldFactor);
+        adjustableRateLimiter.setTimeGritSecond(timeGritSecond);
+        adjustableRateLimiter.setRate(permitsPerSecond);
+        return adjustableRateLimiter;
+    }
+
+    private final SleepingStopwatch stopwatch;
+    private volatile Object mutexDoNotUseDirectly;
+
+    private Object mutex() {
+        Object mutex = mutexDoNotUseDirectly;
+        if (mutex == null) {
+            synchronized (this) {
+                mutex = mutexDoNotUseDirectly;
+                if (mutex == null) {
+                    mutexDoNotUseDirectly = mutex = new Object();
+                }
+            }
+        }
+        return mutex;
+    }
+
+    AdjustableRateLimiter(SleepingStopwatch stopwatch) {
+        if (stopwatch == null) {
+            throw new NullPointerException();
+        }
+        this.stopwatch = stopwatch;
+    }
+
+    public final void setRate(double permitsPerSecond) {
+        if (permitsPerSecond <= 0.0 || Double.isNaN(permitsPerSecond)) {
+            throw new IllegalArgumentException("rate must be positive");
+        }
+        synchronized (mutex()) {
+            doSetRate(permitsPerSecond, stopwatch.readMicros());
+        }
+    }
+
+    void doSetRate(double permitsPerSecond, long nowMicros) {
+
+    }
+
+    public final double getRate() {
+        synchronized (mutex()) {
+            return doGetRate();
+        }
+    }
+
+    public double doGetRate() {
+        return 0;
+    }
+
+    double acquire() {
+        return acquire(1);
+    }
+
+    double acquire(int permits) {
+        long microsToWait = reserve(permits);
+        stopwatch.sleepMicrosUninterruptibly(microsToWait);
+        return 1.0 * microsToWait / SECONDS.toMicros(1L);
+    }
+
+    private long reserve(int permits) {
+        checkPermits(permits);
+        synchronized (mutex()) {
+            return reserveAndGetWaitLength(permits, stopwatch.readMicros());
+        }
+    }
+
+    public boolean tryAcquire(long timeout, TimeUnit unit) {
+        return tryAcquire(1, timeout, unit);
+    }
+
+    public boolean tryAcquire(int permits) {
+        return tryAcquire(permits, 0, MICROSECONDS);
+    }
+
+    public boolean tryAcquire() {
+        return tryAcquire(1, 0, MICROSECONDS);
+    }
+
+    private boolean tryAcquire(int permits, long timeout, TimeUnit unit) {
+        long timeoutMicros = max(unit.toMicros(timeout), 0);
+        checkPermits(permits);
+        long microsToWait;
+
+        // 应对并发情况需要同步
+        synchronized (mutex()) {
+            long nowMicros = stopwatch.readMicros();
+            if (!canAcquire(nowMicros, timeoutMicros)) {
+                return false;
+            } else {
+                // 获得需要等待的时间
+                microsToWait = reserveAndGetWaitLength(permits, nowMicros);
+            }
+        }
+
+        // 等待，当未达到限制时，microsToWait为0
+        stopwatch.sleepMicrosUninterruptibly(microsToWait);
+
+        return true;
+    }
+
+    private boolean canAcquire(long nowMicros, long timeoutMicros) {
+        return queryEarliestAvailable(nowMicros) - timeoutMicros <= nowMicros;
+    }
+
+    private long reserveAndGetWaitLength(int permits, long nowMicros) {
+        long momentAvailable = reserveEarliestAvailable(permits, nowMicros);
+        return max(momentAvailable - nowMicros, 0);
+    }
+
+    /**
+     * The query earliest available
+     *
+     * @param nowMicros now micros
+     */
+    long queryEarliestAvailable(long nowMicros) {
+        return 0L;
+    }
+
+    /**
+     * The reserve earliest available
+     *
+     * @param permits   permits
+     * @param nowMicros now micros
+     */
+    long reserveEarliestAvailable(int permits, long nowMicros) {
+        return 0L;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.ROOT, "RateLimiter[stableRate=%3.1fqps]", getRate());
+    }
+
+    /**
+     *
+     */
+    static class SleepingStopwatch {
+
+        SleepingStopwatch() {
+        }
+
+        protected long readMicros() {
+            return 0L;
+        }
+
+        protected void sleepMicrosUninterruptibly(long micros) {
+
+        }
+
+        static SleepingStopwatch createFromSystemTimer() {
+            return new SleepingStopwatch() {
+                final Stopwatch stopwatch = Stopwatch.createStarted();
+
+                @Override
+                protected long readMicros() {
+                    return stopwatch.elapsed(MICROSECONDS);
+                }
+
+                @Override
+                protected void sleepMicrosUninterruptibly(long micros) {
+                    if (micros > 0) {
+                        Uninterruptibles.sleepUninterruptibly(micros, MICROSECONDS);
+                    }
+                }
+            };
+        }
+    }
+
+    private static void checkPermits(int permits) {
+        if (permits <= 0) {
+            throw new IllegalArgumentException(String.format("Requested permits (%s) must be positive", permits));
+        }
+    }
+
+}
---
+++ neural-limiter/src/main/java/cn/micro/neural/limiter/extension/SmoothRateLimiter.java
@@ -0,0 +1,166 @@
+package cn.micro.neural.limiter.extension;
+
+import static java.lang.Math.min;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import com.google.common.math.LongMath;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * The Smooth Rate Limiter.
+ *
+ * @author lry
+ */
+public abstract class SmoothRateLimiter extends AdjustableRateLimiter {
+
+    static final class SmoothWarmingUp extends SmoothRateLimiter {
+
+        private final long warmUpPeriodMicros;
+        private double slope;
+        private double thresholdPermits;
+        private double coldFactor;
+
+        SmoothWarmingUp(SleepingStopwatch stopwatch, long warmupPeriod, TimeUnit timeUnit, double coldFactor) {
+            super(stopwatch);
+            this.warmUpPeriodMicros = timeUnit.toMicros(warmupPeriod);
+            this.coldFactor = coldFactor;
+        }
+
+        @Override
+        void doSetRate(double permitsPerSecond, double stableIntervalMicros) {
+            double oldMaxPermits = maxPermits;
+            double coldIntervalMicros = stableIntervalMicros * coldFactor;
+
+            thresholdPermits = 0.5 * warmUpPeriodMicros / stableIntervalMicros;
+            maxPermits = thresholdPermits + 2.0 * warmUpPeriodMicros / (stableIntervalMicros + coldIntervalMicros);
+            slope = (coldIntervalMicros - stableIntervalMicros) / (maxPermits - thresholdPermits);
+
+            if (oldMaxPermits == Double.POSITIVE_INFINITY) {
+                storedPermits = 0.0;
+            } else {
+                storedPermits = (oldMaxPermits == 0.0) ? maxPermits : storedPermits * maxPermits / oldMaxPermits;
+            }
+        }
+
+        @Override
+        long storedPermitsToWaitTime(double storedPermits, double permitsToTake) {
+            double availablePermitsAboveThreshold = storedPermits - thresholdPermits;
+            long micros = 0;
+
+            if (availablePermitsAboveThreshold > 0.0) {
+                double permitsAboveThresholdToTake = min(availablePermitsAboveThreshold, permitsToTake);
+                double length = permitsToTime(availablePermitsAboveThreshold) +
+                        permitsToTime(availablePermitsAboveThreshold - permitsAboveThresholdToTake);
+                micros = (long) (permitsAboveThresholdToTake * length / 2.0);
+                permitsToTake -= permitsAboveThresholdToTake;
+            }
+
+            micros += (stableIntervalMicros * permitsToTake);
+
+            return micros;
+        }
+
+        private double permitsToTime(double permits) {
+            return stableIntervalMicros + permits * slope;
+        }
+
+        @Override
+        double coolDownIntervalMicros() {
+            return warmUpPeriodMicros / maxPermits;
+        }
+    }
+
+    static final class SmoothBursty extends SmoothRateLimiter {
+
+        final double maxBurstSeconds;
+
+        SmoothBursty(SleepingStopwatch stopwatch, double maxBurstSeconds) {
+            super(stopwatch);
+            this.maxBurstSeconds = maxBurstSeconds;
+        }
+
+        @Override
+        void doSetRate(double permitsPerSecond, double stableIntervalMicros) {
+            double oldMaxPermits = this.maxPermits;
+            maxPermits = maxBurstSeconds * permitsPerSecond;
+
+            if (oldMaxPermits == Double.POSITIVE_INFINITY) {
+                storedPermits = maxPermits;
+            } else {
+                storedPermits = (oldMaxPermits == 0.0) ? 0.0 : storedPermits * maxPermits / oldMaxPermits;
+            }
+        }
+
+        @Override
+        long storedPermitsToWaitTime(double storedPermits, double permitsToTake) {
+            return 0L;
+        }
+
+        @Override
+        double coolDownIntervalMicros() {
+            return stableIntervalMicros;
+        }
+    }
+
+    double storedPermits;
+    double maxPermits;
+    double stableIntervalMicros;
+    private long nextFreeTicketMicros = 0L;
+
+    private SmoothRateLimiter(SleepingStopwatch stopwatch) {
+        super(stopwatch);
+    }
+
+    @Override
+    final void doSetRate(double permitsPerSecond, long nowMicros) {
+        reSync(nowMicros);
+        double stableIntervalMicros = super.getTimeGritSecond() / permitsPerSecond;
+        this.stableIntervalMicros = stableIntervalMicros;
+        doSetRate(permitsPerSecond, stableIntervalMicros);
+    }
+
+    abstract void doSetRate(double permitsPerSecond, double stableIntervalMicros);
+
+    @Override
+    public final double doGetRate() {
+        return SECONDS.toMicros(1L) / stableIntervalMicros;
+    }
+
+    @Override
+    final long queryEarliestAvailable(long nowMicros) {
+        return nextFreeTicketMicros;
+    }
+
+    @Override
+    final long reserveEarliestAvailable(int requiredPermits, long nowMicros) {
+        // 补充令牌
+        reSync(nowMicros);
+
+        long returnValue = nextFreeTicketMicros;
+        // 获取这次请求消耗的令牌数目
+        double storedPermitsToSpend = min(requiredPermits, this.storedPermits);
+        double freshPermits = requiredPermits - storedPermitsToSpend;
+        long waitMicros = storedPermitsToWaitTime(this.storedPermits,
+                storedPermitsToSpend) + (long) (freshPermits * stableIntervalMicros);
+
+        this.nextFreeTicketMicros = LongMath.saturatedAdd(nextFreeTicketMicros, waitMicros);
+        // 减去消耗的令牌
+        this.storedPermits -= storedPermitsToSpend;
+
+        return returnValue;
+    }
+
+    abstract long storedPermitsToWaitTime(double storedPermits, double permitsToTake);
+
+    abstract double coolDownIntervalMicros();
+
+    private void reSync(long nowMicros) {
+        if (nowMicros > nextFreeTicketMicros) {
+            double newPermits = (nowMicros - nextFreeTicketMicros) / coolDownIntervalMicros();
+            storedPermits = min(maxPermits, storedPermits + newPermits);
+            nextFreeTicketMicros = nowMicros;
+        }
+    }
+
+}
